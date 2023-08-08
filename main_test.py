9a6a008a387ef0c11947819cc80e89817de40e41
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-08 22:19:58 +0200

    ready

--- main_test.py
+++ main_test.py
@@ -1,6 +1,19 @@
 import main
 
-def test(capsys):
-    main.calculate()
+def test_main(monkeypatch, capsys):
+    inputs = iter(['I am doing well!', 'I love programming!'])
+    prompts = []
+
+    def mock_input(prompt):
+        prompts.append(prompt)
+        return next(inputs)
+
+    monkeypatch.setattr('builtins.input', mock_input)
+    main.main()
     captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+    # Check the prompts
+    assert prompts == ['Greetings! How are you doing?', 'Oh, how interesting. Tell me more!']
+
+    # Check the printed output
+    assert captured.out == 'Thanks for sharing!\n'
